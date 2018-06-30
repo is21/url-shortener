19a1a8ad5397cc3f0b8fb66520e8d28fd13609e6
Ian Schiffer <ischiffer@gmail.com>
2018-06-30 07:46:06 -0700

    URLs can be shortened

--- public/main.js
+++ public/main.js
@@ -1,4 +1,29 @@
+const apiKey = '37fb9fc99c6a423984723af859785c16'
+const rebrandlyUrl = 'https://api.rebrandly.com/v1/links'
+
+
 function shortenUrl() {
-  url = document.getElementById('longUrl').value;
-  alert(url);
+  const longUrl = document.getElementById('longUrl').value;
+  const data = JSON.stringify({destination: longUrl})
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
+    console.log(xhr.response);
+    if(xhr.readyState === XMLHttpRequest.DONE){
+      renderResponse(xhr.response);
+    }
+  }
+  xhr.open('POST', rebrandlyUrl, true)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  xhr.setRequestHeader('apikey', apiKey)
+  xhr.send(data);
+}
+
+function renderResponse(res){
+  if(res.errors){
+    responseField.innerHTML = "<p>Your URL couldn't be shortened. Try again, please.</p>"
+  }
+  else {
+    responseField.innerHTML = `<p>Short URL: ${res.shortUrl}</p>`;
+  }
 }
