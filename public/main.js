f62b09d7cda5aadda077a62c324b20bd77b3333c
Ian Schiffer <ischiffer@gmail.com>
2018-07-07 10:55:34 -0700

    add styling and snackbar

--- public/main.js
+++ public/main.js
@@ -1,6 +1,14 @@
 const apiKey = '37fb9fc99c6a423984723af859785c16'
 const rebrandlyUrl = 'https://api.rebrandly.com/v1/links'
 
+function copyToClipBoard() {
+  document.getElementById("shortLink").addEventListener("click", function(){
+    var copyText = document.getElementById("shortLink");
+    copyText.select();
+    document.execCommand("copy");
+    showSnackbar('Short URL copied to clipboard');
+});
+}
 
 function shortenUrl() {
   const longUrl = document.getElementById('longUrl').value;
@@ -27,10 +35,20 @@ function renderResponse(res){
     responseSection.innerHTML = "<p>Your URL couldn't be shortened. Try again, please.</p>"
   }
   else {
-    responseSection.innerHTML = `<p>Your short URL is <b>${res.shortUrl}</b></p>`;
+    responseSection.innerHTML = `<p>Your short URL is <textarea id="shortLink" cols="15"rows="1" readonly>${res.shortUrl}</textarea></p>`;
+    copyToClipBoard();
   }
 }
 
 function displayLoadingMessage() {
   responseSection.innerHTML = "<p>Shortening your URL...</p>"
+}
+
+function showSnackbar(message) {
+  var x = document.getElementById("snackbar");
+  x.innerHTML = message;
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 4000);
 }
